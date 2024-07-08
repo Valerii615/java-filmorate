3d8b20ae848b66c88f2a28c5234071dfd6ecf893
Valerii <Kovaleff615@yandex.ru>
2024-07-08 10:59:33 +0300

    добавление списка друзей, добавления анатаций

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,16 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     Long id;
     String email;
     String login;
     String name;
     LocalDate birthday;
+    Set<Long> friends = new HashSet<>();
 }
