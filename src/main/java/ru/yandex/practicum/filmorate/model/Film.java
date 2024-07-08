bd9a984cde0e289e66077f73708b2401a692ef22
Valerii <Kovaleff615@yandex.ru>
2024-07-08 10:59:17 +0300

    добавление списка лайков, добавления анатаций

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,16 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Film {
     Long id;
     String name;
     String description;
     LocalDate releaseDate;
     int duration;
+    Set<Long> likes = new HashSet<>();
 }
