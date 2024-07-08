beb49fbf80c7bfe217820955887f1eaac414fd50
Valerii <Kovaleff615@yandex.ru>
2024-07-08 11:02:12 +0300

    класс обработчик ошибок и описание ответа

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleValidation (final ValidationException e) {
+        ErrorResponse errorResponse = new ErrorResponse("error" , e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleNotFound(final NotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse("error", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
