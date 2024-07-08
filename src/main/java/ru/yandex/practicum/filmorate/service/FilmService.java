0627da73d4cc3217c634fe4cc2ecb3d76d97d0fb
Valerii <Kovaleff615@yandex.ru>
2024-07-08 11:21:51 +0300

    code-style

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -78,11 +78,3 @@ public class FilmService {
                 .toList();
     }
 }
-
-class FilmComparator implements Comparator<Film> {
-
-    @Override
-    public int compare(Film o1, Film o2) {
-        return o2.getLikes().size() - o1.getLikes().size();
-    }
-}
