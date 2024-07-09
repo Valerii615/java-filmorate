8a4b0a77734735f81b30cf35f9ade1a36091a9b3
Valerii <Kovaleff615@yandex.ru>
2024-07-09 14:59:29 +0300

    адаптация тестов под новый валидатор

--- src/test/java/ru/yandex/practicum/modelValidator/FilmValidatorTests.java
+++ src/test/java/ru/yandex/practicum/modelValidator/FilmValidatorTests.java
@@ -1,22 +1,30 @@
 package ru.yandex.practicum.modelValidator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilmValidatorTests {
     static InMemoryFilmStorage filmStorage;
     static Film film;
+    static Validator validator;
 
     /**
      * инициализация filmController
      */
     @BeforeAll
-    static void preparation() {
+    static void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
         filmStorage = new InMemoryFilmStorage();
     }
 
@@ -26,28 +34,24 @@ public class FilmValidatorTests {
     @Test
     public void checkingTheBoundaryValuesOfTheNameField() {
         film = Film.builder()
+                .name(null)
                 .description("описание")
                 .releaseDate(LocalDate.of(2024, 6, 22))
                 .duration(30)
                 .build();
 
-        try {
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            assertEquals("Название не может быть пустым", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            film.setName("        ");
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            assertEquals("Название не может быть пустым", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            film.setName("Имя");
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(film));
+        assertEquals(1, violationList.size(), "неверное количество исключений");
+        assertEquals("не должно быть пустым", violationList.getFirst().getMessage(), "получено неверное исключение");
+
+        film.setName("   ");
+        List<ConstraintViolation> violationList1 = new ArrayList<>(validator.validate(film));
+        assertEquals(1, violationList1.size(), "неверное количество исключений");
+        assertEquals("не должно быть пустым", violationList1.getFirst().getMessage(), "получено неверное исключение");
+
+        film.setName("Имя");
+        List<ConstraintViolation> violationList2 = new ArrayList<>(validator.validate(film));
+        assertEquals(0, violationList2.size(), "неверное количество исключений");
     }
 
     /**
@@ -57,30 +61,25 @@ public class FilmValidatorTests {
     public void checkingTheBoundaryValuesOfTheDescriptionField() {
         film = Film.builder()
                 .name("Имя")
+                .description(null)
                 .releaseDate(LocalDate.of(2024, 6, 22))
                 .duration(30)
                 .build();
 
-        try {
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
-        try {
-            film.setDescription("Описание");
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
-        try {
-            film.setDescription("Очень длинное описание  " +
-                    "11111111111111111111111111111111111111111111111111111111111" +
-                    "11111111111111111111111111111111111111111111111111111111111" +
-                    "11111111111111111111111111111111111111111111111111111111111");
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            assertEquals("Длина описания не должна превышать — 200 символов, текущая длинна: " + film.getDescription().length(), e.getMessage(), "Получено неверное исключение");
-        }
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(film));
+        assertEquals(1, violationList.size(), "неверное количество исключений");
+        assertEquals("не должно быть пустым", violationList.getFirst().getMessage(), "получено неверное исключение");
+
+        film.setDescription("Очень длинное описание  " +
+                "11111111111111111111111111111111111111111111111111111111111" +
+                "11111111111111111111111111111111111111111111111111111111111" +
+                "11111111111111111111111111111111111111111111111111111111111");
+        List<ConstraintViolation> violationList1 = new ArrayList<>(validator.validate(film));
+        assertEquals("размер должен находиться в диапазоне от 0 до 200", violationList1.getFirst().getMessage(), "получено неверное исключение");
+
+        film.setDescription("обычное описание");
+        List<ConstraintViolation> violationList2 = new ArrayList<>(validator.validate(film));
+        assertEquals(0, violationList2.size(), "неверное количество исключений");
     }
 
     /**
@@ -91,26 +90,20 @@ public class FilmValidatorTests {
         film = Film.builder()
                 .name("Имя")
                 .description("Описание")
+                .releaseDate(null)
                 .duration(30)
                 .build();
 
-        try {
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
-        try {
-            film.setReleaseDate(LocalDate.of(2024, 6, 22));
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
-        try {
-            film.setReleaseDate(LocalDate.of(1, 1, 1));
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            assertEquals("Дата релиза должна быть не раньше 28 декабря 1895 года", e.getMessage(), "Получено неверное исключение");
-        }
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(film));
+        assertEquals(0, violationList.size(), "неверное количество исключений");
+
+        film.setReleaseDate(LocalDate.of(2024, 6, 22));
+        List<ConstraintViolation> violationList1 = new ArrayList<>(validator.validate(film));
+        assertEquals(0, violationList1.size(), "неверное количество исключений");
+
+        film.setReleaseDate(LocalDate.of(1, 1, 1));
+        List<ConstraintViolation> violationList3 = new ArrayList<>(validator.validate(film));
+        assertEquals("Date must not be before 1895-12-28", violationList3.getFirst().getMessage(), "получено неверное исключение");
     }
 
     /**
@@ -122,23 +115,18 @@ public class FilmValidatorTests {
                 .name("Имя")
                 .description("Описание")
                 .releaseDate(LocalDate.of(2024, 6, 22))
+                .duration(0)
                 .build();
-        try {
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            assertEquals("Продолжительность фильма должна быть положительным числом", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            film.setDuration(-30);
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            assertEquals("Продолжительность фильма должна быть положительным числом", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            film.setDuration(30);
-            filmStorage.validator(film);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
+
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(film));
+        assertEquals(0, violationList.size(), "неверное количество исключений");
+
+        film.setDuration(-30);
+        List<ConstraintViolation> violationList2 = new ArrayList<>(validator.validate(film));
+        assertEquals("должно быть больше или равно 0", violationList2.getFirst().getMessage(), "получено неверное исключение");
+
+        film.setDuration(30);
+        List<ConstraintViolation> violationList3 = new ArrayList<>(validator.validate(film));
+        assertEquals(0, violationList3.size(), "неверное количество исключений");
     }
 }
--- src/test/java/ru/yandex/practicum/modelValidator/UserValidatorTests.java
+++ src/test/java/ru/yandex/practicum/modelValidator/UserValidatorTests.java
@@ -1,23 +1,32 @@
 package ru.yandex.practicum.modelValidator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserValidatorTests {
 
     static InMemoryUserStorage userStorage;
-    User user;
+    static User user;
+    static Validator validator;
+
 
     /**
      * инициализация userController
      */
     @BeforeAll
-    static void preparation() {
+    static void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
         userStorage = new InMemoryUserStorage();
     }
 
@@ -27,34 +36,23 @@ public class UserValidatorTests {
     @Test
     public void checkingTheBoundaryValuesOfTheEmailField() {
         user = User.builder()
+                .email(null)
                 .login("login")
                 .name("Имя")
                 .birthday(LocalDate.of(1999, 1, 1))
                 .build();
 
-        try {
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Электронная почта не может быть пустой и должна содержать символ @", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            user.setEmail("       ");
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Электронная почта не может быть пустой и должна содержать символ @", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            user.setEmail("eee.yandex.ru");
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Электронная почта не может быть пустой и должна содержать символ @", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            user.setEmail("eee@yandex.ru");
-            userStorage.validator(user);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(user));
+        assertEquals(1, violationList.size(), "неверное количество исключений");
+        assertEquals("не должно быть пустым", violationList.getFirst().getMessage(), "получено неверное исключение");
+
+        user.setEmail("sdb233");
+        List<ConstraintViolation> violationList2 = new ArrayList<>(validator.validate(user));
+        assertEquals("должно иметь формат адреса электронной почты", violationList2.getFirst().getMessage(), "получено неверное исключение");
+
+        user.setEmail("eee@yandex.ru");
+        List<ConstraintViolation> violationList3 = new ArrayList<>(validator.validate(user));
+        assertEquals(0, violationList3.size(), "неверное количество исключений");
     }
 
     /**
@@ -64,32 +62,24 @@ public class UserValidatorTests {
     public void checkingTheBoundaryValuesOfTheLoginField() {
         user = User.builder()
                 .email("eee@yandex.ru")
+                .login(null)
                 .name("Имя")
                 .birthday(LocalDate.of(1999, 1, 1))
                 .build();
 
-        try {
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Логин не может быть пустым и содержать пробелы", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            user.setLogin("       ");
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Логин не может быть пустым и содержать пробелы", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            user.setLogin("l o g i n");
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Логин не может быть пустым и содержать пробелы", e.getMessage(), "Получено неверное исключение");
-        }
-        try {
-            user.setLogin("login");
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(user));
+        assertEquals(1, violationList.size(), "неверное количество исключений");
+        assertEquals("не должно быть пустым", violationList.getFirst().getMessage(), "получено неверное исключение");
+
+        user.setLogin("login");
+        List<ConstraintViolation> violationList2 = new ArrayList<>(validator.validate(user));
+        assertEquals(0, violationList2.size(), "неверное количество исключений");
+
+        user.setLogin("l o g i n");
+        List<ConstraintViolation> violationList3 = new ArrayList<>(validator.validate(user));
+        assertEquals("значение не должно содержать пробелы", violationList3.getFirst().getMessage(), "получено неверное исключение");
+
+
     }
 
     /**
@@ -116,24 +106,18 @@ public class UserValidatorTests {
                 .email("eee@yandex.ru")
                 .login("login")
                 .name("name")
+                .birthday(null)
                 .build();
 
-        try {
-            userStorage.validator(user);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
-        try {
-            user.setBirthday(LocalDate.of(1999, 1, 1));
-            userStorage.validator(user);
-        } catch (Exception e) {
-            fail("Исключение не должно было появиться");
-        }
-        try {
-            user.setBirthday(LocalDate.of(9999, 9, 9));
-            userStorage.validator(user);
-        } catch (Exception e) {
-            assertEquals("Дата рождения не может быть в будущем", e.getMessage(), "Получено неверное исключение");
-        }
+        List<ConstraintViolation> violationList = new ArrayList<>(validator.validate(user));
+        assertEquals(0, violationList.size(), "неверное количество исключений");
+
+        user.setBirthday(LocalDate.of(1999, 1, 1));
+        List<ConstraintViolation> violationList2 = new ArrayList<>(validator.validate(user));
+        assertEquals(0, violationList2.size(), "неверное количество исключений");
+
+        user.setBirthday(LocalDate.of(9999, 9, 9));
+        List<ConstraintViolation> violationList3 = new ArrayList<>(validator.validate(user));
+        assertEquals("должно содержать прошедшую дату", violationList3.getFirst().getMessage(), "получено неверное исключение");
     }
 }
