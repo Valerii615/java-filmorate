cf36a6b814c94989c18c5f2206dfe696003a196e
Valerii <Kovaleff615@yandex.ru>
2024-07-09 14:59:03 +0300

    реализация собственных анатаций для проверки поля на пробел и на дату

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/NotContainSpace.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.constraints.Past;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = NotContainSpaceValidator.class)
+@Documented
+public @interface NotContainSpace {
+    String message() default "значение не должно содержать пробелы";
+    Class<?>[] groups() default {};
+    Class<?>[] payload() default {};
+}
