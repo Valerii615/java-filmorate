d4275c20cf3f6af49b69d9fa0e34aa87fbd553c1
Valerii <Kovaleff615@yandex.ru>
2024-07-30 10:21:37 +0300

    добавление мапперов для корректной распаковки объектов из базы данных

---
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/FilmRowMapper.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.mappers;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class FilmRowMapper implements RowMapper<Film> {
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Mpa rating = new Mpa();
+        rating.setId(rs.getInt("rating"));
+        return Film.builder()
+                .id(rs.getLong("film_id"))
+                .name(rs.getString("name"))
+                .description(rs.getString("description"))
+                .releaseDate(rs.getDate("releaseDate").toLocalDate())
+                .duration(rs.getInt("duration"))
+                .mpa(rating)
+                .build();
+    }
+}
