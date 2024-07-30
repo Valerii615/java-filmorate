fa2c9acf184ea1f3ca7d5368b8564012a2e7ee6a
Valerii <Kovaleff615@yandex.ru>
2024-07-30 10:21:59 +0300

    добавил исключение

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InternalServerException.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {super(message);}
+}
