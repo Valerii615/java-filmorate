8e4fb8e63ba3fd92658a6431b1244337c2c905f5
Valerii <Kovaleff615@yandex.ru>
2024-07-30 10:24:36 +0300

    добавил и адаптировал контроллеры под новый функционал

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,41 +23,49 @@ public class UserController {
 
     @GetMapping
     public Collection<User> getAllUsers() {
+        log.info("начало обработки эндпоинта получения всех пользователей");
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
     public User getUserOfId(@PathVariable Long id) {
+        log.info("начало обработки эндпоинта получения пользователя по id={}", id);
         return userService.getUserOfId(id);
     }
 
     @GetMapping("/{id}/friends")
     public Collection<User> getFriendsList(@PathVariable Long id) {
+        log.info("начало обработки эндпоинта получения списка друзей id={}", id);
         return userService.getFriendsList(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     public Collection<User> getSharedListOfFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        log.info("начало обработки эндпоинта получения списка общих друзей id={}, otherId={}", id, otherId);
         return userService.getSharedListOfFriends(id, otherId);
     }
 
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
+        log.info("начало обработки эндпоинта добавления пользователя");
         return userService.addUser(user);
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User newUser) {
+        log.info("начало обработки эндпоинта обнавления пользователя");
         return userService.updateUser(newUser);
     }
 
     @PutMapping("{id}/friends/{friendId}")
     public void addInFriends(@PathVariable Long id, @PathVariable Long friendId) {
+        log.info("начало обработки эндпоинта добавления пользователя в друзья id={}, friendId={}", id, friendId);
         userService.addInFriends(id, friendId);
     }
 
     @DeleteMapping("{id}/friends/{friendId}")
     public void removeFromFriends(@PathVariable Long id, @PathVariable Long friendId) {
+        log.info("начало обработки эндпоинта удаления пользователя из друзей id={}, friendId={}", id, friendId);
         userService.removeFromFriends(id, friendId);
     }
 }
